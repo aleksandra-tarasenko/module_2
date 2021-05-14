80f6cc9de8725e59a2622d920701911b23eb9bc4
aleksandra-tarasenko <tarasenko.sashulya@gmail.com>
2021-05-14 14:54:37 +0300

    class+ define INITIAL_CAP

---
+++ dynamicArray.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Sashulya Tarasenko on 14.05.2021.
+//
+
--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//
+//using namespace std;
+
+//int main() {
+//    string s;
+//    cin >> s;
+//    cout << "Number of 'e':" << count(s.begin(), s.end(), 'e') << endl;
+//    int c = 0;
+//    for (int i = 1; i < s.size(); i++) {
+//        if (s[i - 1] == 'e' && s[i] != 'r')
+//            c++;
+//    }
+//    cout << "Number of 'e' without next 'r':" << c;
+
+
 #include <iostream>
-#include <algorithm>
-#include <string>
 
 using namespace std;
+#define INITIAL_CAPACITY 5
 
-int main() {
-    string s;
-    cin >> s;
-    cout << "Number of 'e':" << count(s.begin(), s.end(), 'e') << endl;
-    int c = 0;
-    for (int i = 1; i < s.size(); i++) {
-        if (s[i - 1] == 'e' && s[i] != 'r')
-            c++;
-    }
-    cout << "Number of 'e' without next 'r':" << c;
+template <class T>
+class dynamyc_array{
+    T*array;
+    int MIN_Capacity = INITIAL_CAPACITY;
 
-
-}
+};
